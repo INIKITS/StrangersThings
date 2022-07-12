5aa2cf901aaec5d87f61efdf5a2f0dfe6a9bb696
Dakota Solis <dakota.h.solis@gmail.com>
2022-07-12 17:53:31 -0500

    Working on fixing routes

--- strangers-things/src/components/index.js
+++ strangers-things/src/components/index.js
@@ -1,5 +1,7 @@
+export { default as Home} from "./Home";
 export { default as Nav } from "./Nav";
 export { default as Posts } from "./Posts";
 export { default as NewUserForm } from "./NewUserForm";
 export { default as NewPostForm } from "./NewPostForm";
+export {default as LoginForm} from "./LoginForm";
 export { default as Footer } from "./Footer";
--- strangers-things/src/index.js
+++ strangers-things/src/index.js
@@ -1,21 +1,28 @@
 import React from "react";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router} from "react-router-dom";
-import {Route, Routes} from "react-router-dom"
-import { Nav, Posts, NewUserForm, NewPostForm, Footer } from "./components";
-import LoginForm from "./components/LoginForm";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import {
+  Home,
+  Nav,
+  Posts,
+  LoginForm,
+  NewUserForm,
+  NewPostForm,
+  Footer,
+} from "./components";
 
 const App = () => {
-  const [username,setUsername] = useState('');
-  const [password, setPassword] = useState('')
-  const [userToken, setUserToken] = useState('')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [userToken, setUserToken] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [newTitle, setNewTitle] = useState('')
-  const [newBody, setNewBody] = useState('');
+  const [newTitle, setNewTitle] = useState("");
+  const [newBody, setNewBody] = useState("");
 
-  console.log('userToken', userToken)
-  console.log('isLoggedIn', isLoggedIn)
+  console.log("userToken", userToken);
+  console.log("isLoggedIn", isLoggedIn);
 
   // useEffect(() => {
   //   const checkIsLoggedIn = () => {
@@ -30,25 +37,36 @@ const App = () => {
 
   // }},[])
 
-
   return (
-    
     <>
-      < Nav isLoggedIn = {isLoggedIn} setIsLoggedIn={setIsLoggedIn} setUserToken={setUserToken} />
-    < Routes>
-    {/* <Route exact path='/' element={<NewUserForm />}></Route> */}
-      {/* <div id='main-content'> */}
-     <Route  path='/' element={<Posts/>}></Route>
-      {/* </div> */}
-      {/* <div id='sidebar'> */}
-      {isLoggedIn ? <Route path='/new-post' element={<NewPostForm/>}></Route> : null}
-      <Route path='/' element={<NewUserForm/>} />
-      {/* {isLoggedIn  ? <NewPostForm setNewTitle={setNewTitle} setNewBody={setNewBody}/> : <NewUserForm setUserToken={setUserToken} setIsLoggedIn = {setIsLoggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>} */}
-      <Route  path='/login' element={<LoginForm setUsername={setUsername} setPassword={setPassword}/>} ></Route>
-      {/* </div> */}
+      <Nav
+        isLoggedIn={isLoggedIn}
+        setIsLoggedIn={setIsLoggedIn}
+        setUserToken={setUserToken}
+      />
+      <Routes>
+        <Route exact path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+        {/* <div id='main-content'> */}
+        {/* <Route  exact path='/posts' element={<><Posts/> <NewPostForm/> </>}></Route> */}
+        {/* </div> */}
+        {/* <div id='sidebar'> */}
+        {isLoggedIn ? (
+          <Route path="/new-post" element={<NewPostForm />}></Route>
+        ) : null}
+        <Route exact path="/" element={<NewUserForm />} />
+        {/* {isLoggedIn  ? <NewPostForm setNewTitle={setNewTitle} setNewBody={setNewBody}/> : <NewUserForm setUserToken={setUserToken} setIsLoggedIn = {setIsLoggedIn} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>} */}
+        <Route
+          path="/login"
+          element={
+            <LoginForm setUsername={setUsername} setPassword={setPassword} />
+          }
+        ></Route>
+        {/* </div> */}
       </Routes>
-     <Footer />
-      </>
+      <Footer />
+    </>
   );
 };
 
@@ -56,7 +74,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>
 );
